c99aef5d998536fb45255a17c713792bd64c2156
Johnathan Nguyen <haungocvo@gmail.com>
2021-02-27 13:49:32 -0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+var imageUrls = [];
+const $gallery = document.querySelector("main");
+const $imageUrlInput = document.querySelector("input");
+const $addImageButton = document.querySelector("button");
+
+$addImageButton.addEventListener("click", function () {
+  if ($imageUrlInput.value !== "") {
+    imageUrls.push($imageUrlInput.value);
+  }
+  $imageUrlInput.value = "";
+  updateGallery();
+});
+
+function updateGallery() {
+  $gallery.innerHTML = "";
+  for (let i = 0; i < imageUrls.length; i++) {
+    let $imageElement = document.createElement("img");
+    $imageElement.className = "gallery-image";
+    $imageElement.src = imageUrls[i];
+    $gallery.appendChild($imageElement);
+  }
+}
+updateGallery();
